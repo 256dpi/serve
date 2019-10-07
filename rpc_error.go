8d1777ca514687e45e2620e5b77d5f55436dec09
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-07 14:24:25 +0300

    added rpc

---
+++ rpc_error.go
@@ -0,0 +1,82 @@
+package serve
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+// RPCError objects provide additional information about problems encountered
+// while performing an RPC operation.
+type RPCError struct {
+	// A unique identifier for this particular occurrence of the problem.
+	ID string `json:"id,omitempty"`
+
+	// An URL that leads to further details about this problem.
+	Link string `json:"link,omitempty"`
+
+	// The HTTP status code applicable to this problem.
+	Status int `json:"status,string,omitempty"`
+
+	// An application-specific error code.
+	Code string `json:"code,omitempty"`
+
+	// A short, human-readable summary of the problem.
+	Title string `json:"title,omitempty"`
+
+	// A human-readable explanation specific to this occurrence of the problem.
+	Detail string `json:"detail,omitempty"`
+
+	// A JSON pointer specifying the source of the error.
+	//
+	// See https://tools.ietf.org/html/rfc6901.
+	Source string `json:"source,omitempty"`
+
+	// Non-standard meta-information about the error.
+	Meta map[string]interface{} `json:"meta,omitempty"`
+}
+
+// RPCError returns a string representation of the error for logging purposes.
+func (e *RPCError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Title, e.Detail)
+}
+
+// RPCErrorFromStatus will return an error that has been derived from the passed
+// status code.
+//
+// Note: If the passed status code is not a valid HTTP status code, an Internal
+// Server RPCError status code will be used instead.
+func RPCErrorFromStatus(status int, detail string) *RPCError {
+	// get text
+	str := strings.ToLower(http.StatusText(status))
+
+	// check text
+	if str == "" {
+		status = http.StatusInternalServerError
+		str = strings.ToLower(http.StatusText(http.StatusInternalServerError))
+	}
+
+	return &RPCError{
+		Status: status,
+		Title:  str,
+		Detail: detail,
+	}
+}
+
+// RPCNotFound returns a new not found error.
+func RPCNotFound(detail string) *RPCError {
+	return RPCErrorFromStatus(http.StatusNotFound, detail)
+}
+
+// BadRequestPointer returns a new bad request error with a source.
+func RPCBadRequest(detail, source string) *RPCError {
+	err := RPCErrorFromStatus(http.StatusBadRequest, detail)
+	err.Source = source
+
+	return err
+}
+
+// RPCInternalServerError returns na new internal server error.
+func RPCInternalServerError(detail string) *RPCError {
+	return RPCErrorFromStatus(http.StatusInternalServerError, detail)
+}
---
+++ rpc_handler.go
@@ -0,0 +1,79 @@
+package serve
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// RPCContext allows handlers to interact with the request.
+type RPCContext struct {
+	r *http.Request
+	w http.ResponseWriter
+}
+
+// Request will return the original request.
+func (c *RPCContext) Request() *http.Request {
+	return c.r
+}
+
+// Response will return the associated response writer.
+func (c *RPCContext) ResponseWriter() http.ResponseWriter {
+	return c.w
+}
+
+// Parse will parse the request to the specified value.
+func (c *RPCContext) Parse(v interface{}) error {
+	return json.NewDecoder(c.r.Body).Decode(&v)
+}
+
+// Handle will parse the request and yield control to the callback.
+func (c *RPCContext) Handle(v interface{}, cb func() interface{}) interface{} {
+	// parse request
+	err := c.Parse(v)
+	if err != nil {
+		return err
+	}
+
+	return cb()
+}
+
+// RPCHandler wraps a handler to simplify handling request and responses.
+func RPCHandler(handler func(*RPCContext) interface{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// run handler
+		res := handler(&RPCContext{r: r, w: w})
+		if err, ok := res.(error); ok {
+			// check error
+			rpcError, ok := err.(*RPCError)
+			if !ok {
+				rpcError = RPCInternalServerError("unknown error")
+			}
+
+			// set status
+			if http.StatusText(rpcError.Status) == "" {
+				rpcError.Status = http.StatusInternalServerError
+			}
+
+			// write header
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(rpcError.Status)
+
+			// write error
+			_ = json.NewEncoder(w).Encode(rpcError)
+
+			return
+		}
+
+		// handle nil response
+		if res == nil {
+			res = RPCData(nil)
+		}
+
+		// write header
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		// write response
+		_ = json.NewEncoder(w).Encode(res)
+	}
+}
