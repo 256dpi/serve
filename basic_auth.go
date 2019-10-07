33adfd368ad880e5318b6a6117ca4ce15320c881
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-10-07 12:48:26 +0300

    added basic auth

---
+++ basic_auth.go
@@ -0,0 +1,35 @@
+package serve
+
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"net/http"
+)
+
+// BasicAuth returns a middleware that enforces standard HTTP Basic Authentication.
+func BasicAuth(username, password, realm string) func(http.Handler) http.Handler {
+	// hash username and password
+	requiredUser := sha256.Sum256([]byte(username))
+	requiredPass := sha256.Sum256([]byte(password))
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// get username and password
+			username, password, ok := r.BasicAuth()
+
+			// hash username and password
+			givenUser := sha256.Sum256([]byte(username))
+			givenPass := sha256.Sum256([]byte(password))
+
+			// call next handler if ok
+			if ok && subtle.ConstantTimeCompare(givenUser[:], requiredUser[:]) == 1 && subtle.ConstantTimeCompare(givenPass[:], requiredPass[:]) == 1 {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			// otherwise require authentication
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(http.StatusUnauthorized)
+		})
+	}
+}
---
+++ basic_auth_test.go
@@ -0,0 +1,40 @@
+package serve
+
+import (
+	"encoding/base64"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicAuth(t *testing.T) {
+	ba := Compose(
+		BasicAuth("foo", "bar", "Test"),
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("Protected"))
+		}),
+	)
+
+	r := testRequest(ba, "GET", "/foo", nil, "")
+	assert.Equal(t, "", r.Body.String())
+	assert.Equal(t, http.Header{
+		"Www-Authenticate": []string{`Basic realm="Test"`},
+	}, r.Header())
+
+	r = testRequest(ba, "GET", "/foo", map[string]string{
+		"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("foo:foo")),
+	}, "")
+	assert.Equal(t, "", r.Body.String())
+	assert.Equal(t, http.Header{
+		"Www-Authenticate": []string{`Basic realm="Test"`},
+	}, r.Header())
+
+	r = testRequest(ba, "GET", "/foo", map[string]string{
+		"Authorization": "Basic " + base64.StdEncoding.EncodeToString([]byte("foo:bar")),
+	}, "")
+	assert.Equal(t, "Protected", r.Body.String())
+	assert.Equal(t, http.Header{
+		"Content-Type": []string{"text/plain; charset=utf-8"},
+	}, r.Header())
+}
