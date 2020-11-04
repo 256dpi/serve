016104081de3c73dc030bb80d98e8c54f3163aa1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2020-11-04 18:55:45 +0100

    added default cors policy

--- cors.go
+++ cors.go
@@ -13,3 +13,20 @@ type CORSPolicy = cors.Options
 func CORS(policy CORSPolicy) func(http.Handler) http.Handler {
 	return cors.New(policy).Handler
 }
+
+// CORSDefault returns a default cors policy for basic APIs.
+func CORSDefault(headers ...string) CORSPolicy {
+	return CORSPolicy{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{
+			"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS",
+		},
+		AllowedHeaders: append([]string{
+			"Accept", "Authorization", "Content-Type", "Origin",
+			"X-Requested-With",
+		}, headers...),
+		ExposedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+		MaxAge:           300, // 5 minutes
+	}
+}
